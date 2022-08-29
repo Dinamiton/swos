29e41dfb389bb69cb67155833fb6cb29215335be
Pedro Millan <pedro.millan@enreach.com>
2022-08-29 20:55:56 +0200

    install nelmio

---
+++ annotation.md
@@ -0,0 +1,7 @@
+# Some interesting notes while implementing the project
+
+# Libraries:
+* nelmio:
+`$ composer require nelmio/api-doc-bundle`
+`$ composer require symfony/twig-bundle`
+`$ composer require symfony/asset`
--- config/bundles.php
+++ config/bundles.php
@@ -6,4 +6,6 @@ return [
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
+    Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
 ];
